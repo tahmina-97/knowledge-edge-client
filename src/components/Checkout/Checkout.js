3220d15c075a5b0086fd2fb06a0300d4cc01460a
Tahmina Ria <tahminariya459@gmail.com>
2022-10-27 22:57:07 +0600

    correction

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -8,6 +8,7 @@ const Checkout = () => {
             <h1>{course.course_name}</h1>
             <h2>Author: {course.author.name}</h2>
             <img src={course.author.img} alt="" />
+            <button className='btn btn-primary d-block mx-auto my-4'>Buy now</button>
 
 
         </div>
--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -3,7 +3,7 @@ import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Course = ({ course }) => {
-    const { id, rating, total_purchase, course_name, author, image_url } = course;
+    const { id, rating, total_purchase, course_name, author, image_url, price } = course;
     return (
         <div>
             <div class="col">
@@ -11,12 +11,17 @@ const Course = ({ course }) => {
                     <img src={image_url} class="card-img-top" height="230px" alt="..." />
                     <div class="card-body">
                         <h5 class="card-title">{course_name}</h5>
+
                         <div>
-                            <p>Author: <span className='fw-semibold'>{author.name}</span></p>
+                            <div className='d-flex justify-content-between'>
+                                <p>Author: <span className='fw-semibold'>{author.name}</span></p>
+                                <h5 className='card-title text-danger'>
+                                    $<span className=" text-primary font-bold"> {price}</span></h5>
+
+                            </div>
                             <div className='d-flex justify-content-between'>
                                 <p>Ratings: <span className='fw-bold text-warning'>{rating}</span></p>
                                 <p>Purchased: <span className='fw-semibold'>{total_purchase}</span> times</p>
-
                             </div>
                         </div>
                         <Link to={`/course/${id}`} className='btn btn-primary w-100'>Get Details</Link>

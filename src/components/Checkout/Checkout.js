c668c0b26766757f9e1b3fbe6c2008acc7048c40
Tahmina Ria <tahminariya459@gmail.com>
2022-10-27 22:42:27 +0600

    correction

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Checkout = () => {
-    const { id, course_name, author } = useLoaderData;
+    const course = useLoaderData();
     return (
-        <div>
-            {/* <h1>{course_name}</h1>
-            <h2>Author: {author.name}</h2>
-            <img src={author.img} alt="" /> */}
-            <h1>data</h1>
+        <div className='text-center'>
+            <h1>{course.course_name}</h1>
+            <h2>Author: {course.author.name}</h2>
+            <img src={course.author.img} alt="" />
+
 
         </div>
     );
